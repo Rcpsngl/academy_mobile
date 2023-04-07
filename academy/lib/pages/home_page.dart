3d45bae1d746c66a0e3f217645b05bf590e5f7f2
Recep <06recepsengul@gmail.com>
2023-04-07 23:32:04 +0300

    home page update

--- academy/lib/pages/home_page.dart
+++ academy/lib/pages/home_page.dart
@@ -1,175 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ForumScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // Sekme sayısı
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: const Color(0xff03dac6),
-          child: Icon(Icons.add),
-          foregroundColor: Colors.black,
-          onPressed: () {},
-        ),
-        appBar: AppBar(
-          backgroundColor: const Color(0xff03dac6),
-          title: Text('Forum'),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Sorular'),
-              Tab(text: 'Sosyal'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            ForumTab(
-              title: 'Sorular',
-              posts: [
-                ForumPost(
-                  username: 'Kullanıcı Adı 1',
-                  postTitle: 'Başlık 1',
-                  postContent: 'İçerik 1',
-                  postDate: '4 Nisan 2023',
-                ),
-                ForumPost(
-                  username: 'Kullanıcı Adı 2',
-                  postTitle: 'Başlık 2',
-                  postContent: 'İçerik 2',
-                  postDate: '5 Nisan 2023',
-                ),
-              ],
-            ),
-            ForumTab(
-              title: 'Sosyal',
-              posts: [
-                ForumPost(
-                  username: 'Kullanıcı Adı 3',
-                  postTitle: 'Başlık 3',
-                  postContent: 'İçerik 3',
-                  postDate: '6 Nisan 2023',
-                ),
-                ForumPost(
-                  username: 'Kullanıcı Adı 4',
-                  postTitle: 'Başlık 4',
-                  postContent: 'İçerik 4',
-                  postDate: '7 Nisan 2023',
-                ),
-                ForumPost(
-                  username: 'Kullanıcı Adı 5',
-                  postTitle: 'Başlık 5',
-                  postContent: 'İçerik 4',
-                  postDate: '7 Nisan 2023',
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ForumTab extends StatelessWidget {
-  final String title;
-  final List<ForumPost> posts;
-
-  ForumTab({
-    required this.title,
-    required this.posts,
-  });
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: posts.length,
-      itemBuilder: (BuildContext context, int index) {
-        return ForumCard(
-          username: posts[index].username,
-          postTitle: posts[index].postTitle,
-          postContent: posts[index].postContent,
-          postDate: posts[index].postDate,
-        );
-      },
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class ForumPost {
-  final String username;
-  final String postTitle;
-  final String postContent;
-  final String postDate;
+class _HomePageState extends State<HomePage> {
+// text fields' controllers
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _text = TextEditingController();
 
-  ForumPost({
-    required this.username,
-    required this.postTitle,
-    required this.postContent,
-    required this.postDate,
-  });
-}
+  final CollectionReference _products =
+      FirebaseFirestore.instance.collection('products');
 
-class ForumCard extends StatelessWidget {
-  final String username;
-  final String postTitle;
-  final String postContent;
-  final String postDate;
-
-  ForumCard({
-    required this.username,
-    required this.postTitle,
-    required this.postContent,
-    required this.postDate,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CircleAvatar(
-                  child: Text(
-                    username[0],
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  backgroundColor: Colors.blue,
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Name'),
                 ),
-                SizedBox(width: 8),
-                Text(
-                  username,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+                TextField(
+                  controller: _text,
+                  decoration: const InputDecoration(
+                    labelText: 'Text',
                   ),
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text('Create'),
+                  onPressed: () async {
+                    final String name = _nameController.text;
+                    final String text = _text.text;
+                    if (text.isNotEmpty) {
+                      await _products.add({"name": name, "text": text});
+
+                      _nameController.text = '';
+                      _text.text = '';
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
               ],
             ),
-            SizedBox(height: 8),
-            Text(
-              postTitle,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+          );
+        });
+  }
+
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+      _nameController.text = documentSnapshot['name'];
+      _text.text = documentSnapshot['text'];
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                ),
+                TextField(
+                  controller: _text,
+                  decoration: const InputDecoration(
+                    labelText: 'Text',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text('Update'),
+                  onPressed: () async {
+                    final String name = _nameController.text;
+                    final String text = _text.text;
+                    if (text.isNotEmpty) {
+                      await _products
+                          .doc(documentSnapshot!.id)
+                          .update({"name": name, "text": text});
+                      _nameController.text = '';
+                      _text.text = '';
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
             ),
-            SizedBox(height: 8),
-            Text(postContent),
-            SizedBox(height: 8),
-            Text(
-              postDate,
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-            ),
-          ],
+          );
+        });
+  }
+
+  Future<void> _delete(String productId) async {
+    await _products.doc(productId).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('You have successfully deleted a product')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Forum')),
         ),
-      ),
-    );
+        body: StreamBuilder(
+          stream: _products.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text(documentSnapshot['name']),
+                      subtitle: Text(documentSnapshot['text']),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () => _update(documentSnapshot)),
+                            IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () => _delete(documentSnapshot.id)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+// Add new product
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _create(),
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
