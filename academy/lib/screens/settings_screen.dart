ca3a35cd9b97f50099931f075cf86416cd148ed9
osmanoscar <osmankok701@gmail.com>
2023-04-08 04:07:05 +0300

    settingyÃuklenmedi

---
+++ academy/lib/screens/settings_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../models/setting.dart';
+import '../widgets/avatar_card.dart';
+import '../widgets/setting_tile.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const AvatarCard(),
+                const SizedBox(height: 20),
+                const Divider(),
+                const SizedBox(height: 10),
+                Column(
+                  children: List.generate(
+                    settings.length,
+                    (index) => SettingTile(setting: settings[index]),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Divider(),
+                const SizedBox(height: 10),
+                Column(
+                  children: List.generate(
+                    settings2.length,
+                    (index) => SettingTile(setting: settings2[index]),
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
